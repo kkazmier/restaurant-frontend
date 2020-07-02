2835bf92759109fb5ef10ba63366706390e57e6a
kkazm <s14005@pjwstk.edu.pl>
2020-07-02 22:21:46 +0200

    add Ingredient and Dish domain classes

---
+++ src/main/java/com/kodilla/restaurantfrontend/domain/Dish.java
@@ -0,0 +1,5 @@
+package com.kodilla.restaurantfrontend.domain;
+
+public class Dish {
+
+}
---
+++ src/main/java/com/kodilla/restaurantfrontend/domain/Ingredient.java
@@ -0,0 +1,11 @@
+package com.kodilla.restaurantfrontend.domain;
+
+public class Ingredient {
+    private Long id;
+    private String name;
+    private String type;
+    private String quantity;
+    private String measureUnit;
+    private String price;
+    private String description;
+}
