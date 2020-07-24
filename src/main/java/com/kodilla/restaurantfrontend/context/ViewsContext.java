b36c1de2853f6822df3846c07c7f9e2a9adc224e
kkazm <s14005@pjwstk.edu.pl>
2020-07-24 19:51:48 +0200

    create ViewsContext

--- src/main/java/com/kodilla/restaurantfrontend/context/ViewsContext.java
+++ src/main/java/com/kodilla/restaurantfrontend/context/ViewsContext.java
@@ -1,8 +1,31 @@
 package com.kodilla.restaurantfrontend.context;
 
+import com.kodilla.restaurantfrontend.domain.Dish;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ViewsContext {
+@Getter
+@Setter
+public final class ViewsContext {
+    private static ViewsContext viewsContextInstance = null;
 
+    private Dish selectedDishInDishView;
+
+    private ViewsContext(){
+
+    }
+
+    public static ViewsContext getInstance(){
+        if(viewsContextInstance == null){
+            synchronized (ViewsContext.class){
+                if (viewsContextInstance == null){
+                    viewsContextInstance = new ViewsContext();
+                }
+            }
+        }
+        return viewsContextInstance;
+    }
 }
