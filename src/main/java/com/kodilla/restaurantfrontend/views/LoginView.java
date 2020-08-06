6031e008f3700b8484d87b0a5a77a562fefcfa54
kkazm <s14005@pjwstk.edu.pl>
2020-08-06 18:56:59 +0200

    Implements 'keyboard' in LoginView.

--- src/main/java/com/kodilla/restaurantfrontend/views/LoginView.java
+++ src/main/java/com/kodilla/restaurantfrontend/views/LoginView.java
@@ -25,6 +25,7 @@ public class LoginView extends VerticalLayout {
     private HorizontalLayout firstRow = new HorizontalLayout(one, two, three);
     private HorizontalLayout secondRow = new HorizontalLayout(four, five, six);
     private HorizontalLayout thirdRow = new HorizontalLayout(seven, eight, nine);
+    private String login;
 
     public LoginView() {
         loginLabel.getStyle().set("fontWeight", "bold");
@@ -37,11 +38,83 @@ public class LoginView extends VerticalLayout {
                 loginField,
                 applyBtn
         );
+        login = "";
     }
 
     public void addClickListeners(){
-
+        one.addClickListener(
+                e -> one.getUI().ifPresent(
+                        ui -> {
+                            login += "1";
+                            refresh();
+                        }
+        ));
+        two.addClickListener(
+                e -> two.getUI().ifPresent(
+                        ui -> {
+                            login += "2";
+                            refresh();
+                        }
+                ));
+        three.addClickListener(
+                e -> three.getUI().ifPresent(
+                        ui -> {
+                            login += "3";
+                            refresh();
+                        }
+                ));
+        four.addClickListener(
+                e -> four.getUI().ifPresent(
+                        ui -> {
+                            login += "4";
+                            refresh();
+                        }
+                ));
+        five.addClickListener(
+                e -> five.getUI().ifPresent(
+                        ui -> {
+                            login += "5";
+                            refresh();
+                        }
+                ));
+        six.addClickListener(
+                e -> six.getUI().ifPresent(
+                        ui -> {
+                            login += "6";
+                            refresh();
+                        }
+                ));
+        seven.addClickListener(
+                e -> seven.getUI().ifPresent(
+                        ui -> {
+                            login += "7";
+                            refresh();
+                        }
+                ));
+        eight.addClickListener(
+                e -> eight.getUI().ifPresent(
+                        ui -> {
+                            login += "8";
+                            refresh();
+                        }
+                ));
+        nine.addClickListener(
+                e -> nine.getUI().ifPresent(
+                        ui -> {
+                            login += "9";
+                            refresh();
+                        }
+                ));
+        applyBtn.addClickListener(
+                e -> applyBtn.getUI().ifPresent(
+                        ui -> {
+                            login = "";
+                            refresh();
+                        }
+        ));
     }
 
-
+    public void refresh(){
+        loginField.setValue(login);
+    }
 }
